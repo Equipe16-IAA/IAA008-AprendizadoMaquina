acaf067365b8c76f6d50815e8d4d395e415b2489
Marlon Prudente <marlonmateuspr@gmail.com>
2024-07-21 11:26:52 -0300

    Commit database "Admissão" do Trabalho Final

---
+++ Codigo_TrabalhoFinal/Admissao/rna_holdout.R
@@ -0,0 +1,46 @@
+### Pacotes necessários
+#install.packages("e1071")
+#install.packages("caret")
+library("caret")  
+library(Metrics)
+
+
+
+##Leitura da database
+setwd("D:/GIT/IAA008-AprendizadoMaquina/")
+
+dados <- read.csv("databases/9 - Admissao - Dados.csv", header = T)
+dados$num <- NULL
+
+#View(dados)
+
+
+#(Ano atual com 4 dígitos + 2 algarismos do dígito verificador do CPF de um dos integrantes)
+set.seed(2034)
+
+
+ind <-createDataPartition(dados$ChanceOfAdmit, p=0.80, list=FALSE)
+treino <-dados[ind,]
+teste <-dados[-ind,]
+
+
+##executa o RNA com esse grid
+
+rna <- train(ChanceOfAdmit~.,data = treino, method = "nnet",trace=FALSE)
+
+rna
+
+##Aplica o modelo no arquivo de teste
+predict.rna <- predict(rna,teste)
+
+##mostra as métricas
+
+rmse(teste$ChanceOfAdmit, predict.rna)
+mae(teste$ChanceOfAdmit, predict.rna)
+cor(teste$ChanceOfAdmit, predict.rna, method = "pearson")
+#syx <- function(observados, estimados, n, p){ return sqrt((sum((observados-estimados)^2))/(n-p-1)) }
+#syx(teste$ChanceOfAdmit, predict.rna)
+r2 <- function(predito, observado){ return (1 - (sum((predito-observado)^2)/sum((observado-mean(observado))^2)))}
+
+r2(predict.rna, teste$ChanceOfAdmit)
+
---
+++ Codigo_TrabalhoFinal/Admissao/svm_holdout.R
@@ -0,0 +1,45 @@
+### Pacotes necessários
+#install.packages("e1071")
+#install.packages("caret")
+library("caret")  
+library(Metrics)
+
+
+
+##Leitura da database
+setwd("D:/GIT/IAA008-AprendizadoMaquina/")
+
+dados <- read.csv("databases/9 - Admissao - Dados.csv", header = T)
+dados$num <- NULL
+
+#View(dados)
+
+
+#(Ano atual com 4 dígitos + 2 algarismos do dígito verificador do CPF de um dos integrantes)
+set.seed(2034)
+
+
+ind <-createDataPartition(dados$ChanceOfAdmit, p=0.80, list=FALSE)
+treino <-dados[ind,]
+teste <-dados[-ind,]
+##executa o SVM com esse grid
+
+svm <- train(ChanceOfAdmit~.,data = treino, method = "svmRadial")
+
+svm
+
+##Aplica o modelo no arquivo de teste
+predict.svm <- predict(svm,teste)
+
+##mostra as métricas
+
+rmse(teste$ChanceOfAdmit, predict.svm)
+mae(teste$ChanceOfAdmit, predict.svm)
+cor(teste$ChanceOfAdmit, predict.svm, method = "pearson")
+#syx <- function(observados, estimados, n, p){ return sqrt((sum((observados-estimados)^2))/(n-p-1)) }
+#syx(teste$ChanceOfAdmit, predict.svm)
+r2 <- function(predito, observado){ return (1 - (sum((predito-observado)^2)/sum((observado-mean(observado))^2)))}
+
+r2(predict.svm, teste$ChanceOfAdmit)
+
+
